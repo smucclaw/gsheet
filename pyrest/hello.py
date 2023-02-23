8958166145fb173914784728d402767efc6b31fd
Joe Watt <joewatt95@u.nus.edu>
2023-02-23 16:39:34 +0800

    m

--- pyrest/hello.py
+++ pyrest/hello.py
@@ -256,13 +256,17 @@ def processCsv():
     transpiled_term = maude_vis.natural4_file_to_transpiled_term(
       maude_main_mod, textual_natural4_file
     )
-    maude_vis.transpiled_term_to_html_file(
-      maude_main_mod, transpiled_term, 'all *', maude_html_file
-    )
-    # strat = maude_main_mod.parseStrategy('all *')
-    # netwk = maude_vis.term_strat_to_pyvis_netwk(maude_main_mod, transpiled_term, strat)
-    # netwk.show_buttons()
-    # html_str = netwk.generate_html()
+    # maude_vis.transpiled_term_to_html_file(
+    #   maude_main_mod, transpiled_term, 'all *', maude_html_file
+    # )
+    strat = maude_main_mod.parseStrategy('all *')
+    netwk = maude_vis.term_strat_to_pyvis_netwk(maude_main_mod, transpiled_term, strat)
+    netwk.show_buttons()
+    html_str = netwk.generate_html()
+    print('WRITING HTML:', file=os.stderr)
+    print(html_str, file=os.stderr)
+    with open(maude_html_file, 'w+') as fout:
+      fout.write(html_str)
 
     createFiles = "natural4-exe --only tomd --workdir=" + natural4_dir + " --uuiddir=" + uuid + "/" + spreadsheetId + "/" + sheetId + " " + targetPath
     print("hello.py child: calling natural4-exe (slowly) for tomd", file=sys.stderr)
