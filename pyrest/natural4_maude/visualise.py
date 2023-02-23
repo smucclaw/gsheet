3ff560a72a3052c7282c201830b83071aca68fb4
Joe Watt <joewatt95@u.nus.edu>
2023-02-23 17:19:33 +0800

    m

--- pyrest/natural4_maude/visualise.py
+++ pyrest/natural4_maude/visualise.py
@@ -245,7 +245,6 @@ def term_strat_to_nx_graph(mod, term, strat):
   graph = create_graph(
     term = term, strategy = strat,
     purge_fails = 'yes',
-    logic = ''
   )
   print(1234, file=sys.stderr)
   nx_graph = rewrite_graph_to_nx_graph(mod, graph)
