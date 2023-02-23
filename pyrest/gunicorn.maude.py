17a2042c3d0de118727741c77fd32419f9ca7876
Joe Watt <joewatt95@u.nus.edu>
2023-02-23 14:55:54 +0800

    m

--- pyrest/gunicorn.maude.py
+++ pyrest/gunicorn.maude.py
@@ -2,7 +2,7 @@
 # this file is intended to be customized for each installation; change it as you wish
 #
 
-pythonpath = "/home/mengwong/src/smucclaw/gsheet/pyrest/lib/python3.8/site-packages/"
+# pythonpath = "/home/mengwong/src/smucclaw/gsheet/pyrest/lib/python3.8/site-packages/"
 raw_env = ["basedir="       + ".",
            "V8K_WORKDIR="   + "/home/mengwong/wow/much",
            "v8k_startport=" + "8011",
@@ -16,4 +16,4 @@ keyfile    = "/etc/letsencrypt/live/cclaw.legalese.com/privkey.pem"
 
 preload = True
 accesslog = "access_log"
-workers  = 3
+workers  = 5
