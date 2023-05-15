73b9e8ced19b791f4b4fb4e2c8aaf782ef7f17a1
Joe Watt <joewatt95@u.nus.edu>
2023-05-15 10:41:09 +0800

    Fix indentation

--- pyrest/hello.py
+++ pyrest/hello.py
@@ -114,43 +114,43 @@ async def process_csv():
 
   response = {}
 
-    uuid = data['uuid']
-    spreadsheet_id = data['spreadsheet_id']
-    sheet_id = data['sheet_id']
-    target_folder = natural4_dir + "/" + uuid + "/" + spreadsheet_id + "/" + sheet_id + "/"
-    print(target_folder)
-    time_now = datetime.datetime.utcnow().strftime("%Y%m%dT%H%M%S.%fZ")
-    target_file = time_now + ".csv"
-    target_path = target_folder + target_file
-    # if not os.path.exists(target_folder):
-    Path(target_folder).mkdir(parents=True, exist_ok=True)
+  uuid = data['uuid']
+  spreadsheet_id = data['spreadsheet_id']
+  sheet_id = data['sheet_id']
+  target_folder = natural4_dir + "/" + uuid + "/" + spreadsheet_id + "/" + sheet_id + "/"
+  print(target_folder)
+  time_now = datetime.datetime.utcnow().strftime("%Y%m%dT%H%M%S.%fZ")
+  target_file = time_now + ".csv"
+  target_path = target_folder + target_file
+  # if not os.path.exists(target_folder):
+  Path(target_folder).mkdir(parents=True, exist_ok=True)
 
-    with open(target_path, "w") as fout:
-        fout.write(data['csvString'])
+  with open(target_path, "w") as fout:
+    fout.write(data['csvString'])
 
-    # target_path is for CSV data
+  # target_path is for CSV data
 
-  # ---------------------------------------------
-  # call natural4-exe, wait for it to complete. see SECOND RUN below.
-  # ---------------------------------------------
+# ---------------------------------------------
+# call natural4-exe, wait for it to complete. see SECOND RUN below.
+# ---------------------------------------------
 
-    # one can leave out the markdown by adding the --tomd option
-    # one can leave out the ASP by adding the --toasp option
-    create_files = natural4_exe + " --tomd --toasp --workdir=" + natural4_dir + " --uuiddir=" + uuid + "/" + spreadsheet_id + "/" + sheet_id + " " + target_path
-    print("hello.py main: calling natural4-exe (%s)" % (natural4_exe), file=sys.stderr)
-    print("hello.py main: %s" % (create_files), file=sys.stderr)
-    nl4exe = subprocess.run([create_files], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    print("hello.py main: back from fast natural4-exe (took", datetime.datetime.now() - start_time, ")", file=sys.stderr)
-    print("hello.py main: natural4-exe stdout length = %d" % len(nl4exe.stdout.decode('utf-8')), file=sys.stderr)
-    print("hello.py main: natural4-exe stderr length = %d" % len(nl4exe.stderr.decode('utf-8')), file=sys.stderr)
+  # one can leave out the markdown by adding the --tomd option
+  # one can leave out the ASP by adding the --toasp option
+  create_files = natural4_exe + " --tomd --toasp --workdir=" + natural4_dir + " --uuiddir=" + uuid + "/" + spreadsheet_id + "/" + sheet_id + " " + target_path
+  print("hello.py main: calling natural4-exe (%s)" % (natural4_exe), file=sys.stderr)
+  print("hello.py main: %s" % (create_files), file=sys.stderr)
+  nl4exe = subprocess.run([create_files], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  print("hello.py main: back from fast natural4-exe (took", datetime.datetime.now() - start_time, ")", file=sys.stderr)
+  print("hello.py main: natural4-exe stdout length = %d" % len(nl4exe.stdout.decode('utf-8')), file=sys.stderr)
+  print("hello.py main: natural4-exe stderr length = %d" % len(nl4exe.stderr.decode('utf-8')), file=sys.stderr)
 
-    if len(nl4exe.stderr.decode('utf-8')) < 2000:
-        print(nl4exe.stderr.decode('utf-8'))
-    nl4_out = nl4exe.stdout.decode('utf-8')
-    with open(target_folder + time_now + ".err", "w") as fout:
-        fout.write(nl4exe.stderr.decode('utf-8'))
-    with open(target_folder + time_now + ".out", "w") as fout:
-        fout.write(nl4exe.stdout.decode('utf-8'))
+  if len(nl4exe.stderr.decode('utf-8')) < 2000:
+      print(nl4exe.stderr.decode('utf-8'))
+  nl4_out = nl4exe.stdout.decode('utf-8')
+  with open(target_folder + time_now + ".err", "w") as fout:
+      fout.write(nl4exe.stderr.decode('utf-8'))
+  with open(target_folder + time_now + ".out", "w") as fout:
+      fout.write(nl4exe.stdout.decode('utf-8'))
 
   response['nl4_stderr'] = nl4exe.stderr.decode('utf-8')[:20000]
   response['nl4_stdout'] = nl4exe.stdout.decode('utf-8')[:20000]
@@ -159,136 +159,136 @@ async def process_csv():
   # postprocessing: for petri nets: turn the DOT files into PNGs
   # ---------------------------------------------
 
-    uuid_ss_folder = temp_dir + "workdir/" + uuid + "/" + spreadsheet_id + "/" + sheet_id
-    petri_folder = uuid_ss_folder + "/petri/"
-    dot_path = petri_folder + "LATEST.dot"
-    (timestamp, ext) = os.path.splitext(os.readlink(dot_path))
+  uuid_ss_folder = temp_dir + "workdir/" + uuid + "/" + spreadsheet_id + "/" + sheet_id
+  petri_folder = uuid_ss_folder + "/petri/"
+  dot_path = petri_folder + "LATEST.dot"
+  (timestamp, ext) = os.path.splitext(os.readlink(dot_path))
 
-    if not os.path.exists(petri_folder):
-        print("expected to find petri_folder %s but it's not there!" % (petri_folder), file=sys.stderr)
-    else:
-        petri_path_svg = petri_folder + timestamp + ".svg"
-        petri_path_png = petri_folder + timestamp + ".png"
-        small_petri_path = petri_folder + timestamp + "-small.png"
-        print("hello.py main: running: dot -Tpng -Gdpi=150 " + dot_path + " -o " + petri_path_png + " &", file=sys.stderr)
-        os.system("dot -Tpng -Gdpi=72  " + dot_path + " -o " + small_petri_path + " &")
-        os.system("dot -Tpng -Gdpi=150 " + dot_path + " -o " + petri_path_png + " &")
-        os.system("dot -Tsvg           " + dot_path + " -o " + petri_path_svg + " &")
-        try:
-            if os.path.isfile(petri_folder + "LATEST.svg"):       os.unlink(petri_folder + "LATEST.svg")
-            if os.path.isfile(petri_folder + "LATEST.png"):       os.unlink(petri_folder + "LATEST.png")
-            if os.path.isfile(petri_folder + "LATEST-small.png"): os.unlink(petri_folder + "LATEST-small.png")
-            os.symlink(os.path.basename(petri_path_svg), petri_folder + "LATEST.svg")
-            os.symlink(os.path.basename(petri_path_png), petri_folder + "LATEST.png")
-            os.symlink(os.path.basename(small_petri_path), petri_folder + "LATEST-small.png")
-        except Exception as e:
-            print("hello.py main: got some kind of OS error to do with the unlinking and the symlinking",
-                  file=sys.stderr)
-            print("hello.py main: %s" % (e), file=sys.stderr)
+  if not os.path.exists(petri_folder):
+      print("expected to find petri_folder %s but it's not there!" % (petri_folder), file=sys.stderr)
+  else:
+      petri_path_svg = petri_folder + timestamp + ".svg"
+      petri_path_png = petri_folder + timestamp + ".png"
+      small_petri_path = petri_folder + timestamp + "-small.png"
+      print("hello.py main: running: dot -Tpng -Gdpi=150 " + dot_path + " -o " + petri_path_png + " &", file=sys.stderr)
+      os.system("dot -Tpng -Gdpi=72  " + dot_path + " -o " + small_petri_path + " &")
+      os.system("dot -Tpng -Gdpi=150 " + dot_path + " -o " + petri_path_png + " &")
+      os.system("dot -Tsvg           " + dot_path + " -o " + petri_path_svg + " &")
+      try:
+          if os.path.isfile(petri_folder + "LATEST.svg"):       os.unlink(petri_folder + "LATEST.svg")
+          if os.path.isfile(petri_folder + "LATEST.png"):       os.unlink(petri_folder + "LATEST.png")
+          if os.path.isfile(petri_folder + "LATEST-small.png"): os.unlink(petri_folder + "LATEST-small.png")
+          os.symlink(os.path.basename(petri_path_svg), petri_folder + "LATEST.svg")
+          os.symlink(os.path.basename(petri_path_png), petri_folder + "LATEST.png")
+          os.symlink(os.path.basename(small_petri_path), petri_folder + "LATEST-small.png")
+      except Exception as e:
+          print("hello.py main: got some kind of OS error to do with the unlinking and the symlinking",
+                file=sys.stderr)
+          print("hello.py main: %s" % (e), file=sys.stderr)
 
   # ---------------------------------------------
   # postprocessing: for the babyl4 downstream transpilations
   # - call l4 epilog corel4/LATEST.l4
   # ---------------------------------------------
 
-    core_l4_path = uuid_ss_folder + "/corel4/LATEST.l4"
-    epilog_path = uuid_ss_folder + "/epilog"
-    Path(epilog_path).mkdir(parents=True, exist_ok=True)
-    epilog_file = epilog_path + "/" + time_now + ".epilog"
+  core_l4_path = uuid_ss_folder + "/corel4/LATEST.l4"
+  epilog_path = uuid_ss_folder + "/epilog"
+  Path(epilog_path).mkdir(parents=True, exist_ok=True)
+  epilog_file = epilog_path + "/" + time_now + ".epilog"
 
-    print("hello.py main: running: l4 epilog " + core_l4_path + " > " + epilog_file, "&", file=sys.stderr)
-    os.system("l4 epilog " + core_l4_path + " > " + epilog_file + " &")
-    if os.path.isfile(epilog_path + "/LATEST.epilog"): os.unlink(epilog_path + "/LATEST.epilog")
-    os.symlink(time_now + ".epilog", epilog_path + "/LATEST.epilog")
+  print("hello.py main: running: l4 epilog " + core_l4_path + " > " + epilog_file, "&", file=sys.stderr)
+  os.system("l4 epilog " + core_l4_path + " > " + epilog_file + " &")
+  if os.path.isfile(epilog_path + "/LATEST.epilog"): os.unlink(epilog_path + "/LATEST.epilog")
+  os.symlink(time_now + ".epilog", epilog_path + "/LATEST.epilog")
 
-  # ---------------------------------------------
-  # postprocessing: (re-)launch the vue web server
-  # - call v8k up
-  # ---------------------------------------------
+# ---------------------------------------------
+# postprocessing: (re-)launch the vue web server
+# - call v8k up
+# ---------------------------------------------
 
-    v8kargs = ["python", v8k_path,
-               "--workdir=" + v8k_workdir,
-               "up",
-               "--uuid=" + uuid,
-               "--ssid=" + spreadsheet_id,
-               "--sheetid=" + sheet_id,
-               "--startport=" + v8k_startport,
-               uuid_ss_folder + "/purs/LATEST.purs"]
+  v8kargs = ["python", v8k_path,
+              "--workdir=" + v8k_workdir,
+              "up",
+              "--uuid=" + uuid,
+              "--ssid=" + spreadsheet_id,
+              "--sheetid=" + sheet_id,
+              "--startport=" + v8k_startport,
+              uuid_ss_folder + "/purs/LATEST.purs"]
 
-    print("hello.py main: calling %s" % (" ".join(v8kargs)), file=sys.stderr)
-    os.system(" ".join(v8kargs) + "> " + uuid_ss_folder + "/v8k.out")
-    print("hello.py main: v8k up returned", file=sys.stderr)
-    with open(uuid_ss_folder + "/v8k.out", "r") as read_file:
-        v8k_out = read_file.readline();
-    print("v8k.out: %s" % (v8k_out), file=sys.stderr)
+  print("hello.py main: calling %s" % (" ".join(v8kargs)), file=sys.stderr)
+  os.system(" ".join(v8kargs) + "> " + uuid_ss_folder + "/v8k.out")
+  print("hello.py main: v8k up returned", file=sys.stderr)
+  with open(uuid_ss_folder + "/v8k.out", "r") as read_file:
+      v8k_out = read_file.readline();
+  print("v8k.out: %s" % (v8k_out), file=sys.stderr)
 
-    if re.match(r':\d+', v8k_out):  # we got back the expected :8001/uuid/ssid/sid whatever from the v8k call
-        v8k_url = v8k_out.strip()
-        print("v8k up succeeded with: " + v8k_url, file=sys.stderr)
-        response['v8k_url'] = v8k_url
-    else:
-        response['v8k_url'] = None
+  if re.match(r':\d+', v8k_out):  # we got back the expected :8001/uuid/ssid/sid whatever from the v8k call
+      v8k_url = v8k_out.strip()
+      print("v8k up succeeded with: " + v8k_url, file=sys.stderr)
+      response['v8k_url'] = v8k_url
+  else:
+      response['v8k_url'] = None
 
-  # ---------------------------------------------
-  # load in the aasvg index HTML to pass back to sidebar
-  # ---------------------------------------------
+# ---------------------------------------------
+# load in the aasvg index HTML to pass back to sidebar
+# ---------------------------------------------
 
-    with open(uuid_ss_folder + "/aasvg/LATEST/index.html", "r") as read_file:
-        response['aasvg_index'] = read_file.read()
+  with open(uuid_ss_folder + "/aasvg/LATEST/index.html", "r") as read_file:
+      response['aasvg_index'] = read_file.read()
 
-  # ---------------------------------------------
-  # construct other response elements and log run-timings.
-  # ---------------------------------------------
+# ---------------------------------------------
+# construct other response elements and log run-timings.
+# ---------------------------------------------
 
-    response['timestamp'] = timestamp
+  response['timestamp'] = timestamp
 
-    end_time = datetime.datetime.now()
-    elapsed_time = end_time - start_time
+  end_time = datetime.datetime.now()
+  elapsed_time = end_time - start_time
 
-    print("hello.py processCsv ready to return at", end_time, "(total", elapsed_time, ")", file=sys.stderr)
+  print("hello.py processCsv ready to return at", end_time, "(total", elapsed_time, ")", file=sys.stderr)
 
-  # ---------------------------------------------
-  # call natural4-exe; this is the SECOND RUN for any slow transpilers
-  # ---------------------------------------------
+# ---------------------------------------------
+# call natural4-exe; this is the SECOND RUN for any slow transpilers
+# ---------------------------------------------
 
+  print("hello.py processCsv parent returning at ", datetime.datetime.now(), "(total",
+        datetime.datetime.now() - start_time, ")", file=sys.stderr)
+
+  childpid = os.fork()
+  # if this leads to trouble we may need to double-fork with grandparent-wait
+  if childpid > 0:  # in the parent
+    # print("hello.py processCsv parent returning at", datetime.datetime.now(), "(total", datetime.datetime.now() - start_time, ")", file=sys.stderr)
     print("hello.py processCsv parent returning at ", datetime.datetime.now(), "(total",
           datetime.datetime.now() - start_time, ")", file=sys.stderr)
+    # print(json.dumps(response), file=sys.stderr)
 
-    childpid = os.fork()
-    # if this leads to trouble we may need to double-fork with grandparent-wait
-    if childpid > 0:  # in the parent
-        # print("hello.py processCsv parent returning at", datetime.datetime.now(), "(total", datetime.datetime.now() - start_time, ")", file=sys.stderr)
-        print("hello.py processCsv parent returning at ", datetime.datetime.now(), "(total",
-              datetime.datetime.now() - start_time, ")", file=sys.stderr)
-        # print(json.dumps(response), file=sys.stderr)
+    return json.dumps(response)
+  else:  # in the child
+    print("hello.py processCsv: fork(child): continuing to run", file=sys.stderr)
 
-        return json.dumps(response)
-    else:  # in the child
-        print("hello.py processCsv: fork(child): continuing to run", file=sys.stderr)
+    create_files = natural4_exe + " --only tomd --workdir=" + natural4_dir + " --uuiddir=" + uuid + "/" + spreadsheet_id + "/" + sheet_id + " " + target_path
+    print("hello.py child: calling natural4-exe (%s) (slowly) for tomd" % (natural4_exe), file=sys.stderr)
+    print("hello.py child: %s" % (create_files), file=sys.stderr)
+    nl4exe = subprocess.run([create_files], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    print("hello.py child: back from slow natural4-exe 1 (took", datetime.datetime.now() - start_time, ")",
+          file=sys.stderr)
+    print("hello.py child: natural4-exe stdout length = %d" % len(nl4exe.stdout.decode('utf-8')), file=sys.stderr)
+    print("hello.py child: natural4-exe stderr length = %d" % len(nl4exe.stderr.decode('utf-8')), file=sys.stderr)
 
-        create_files = natural4_exe + " --only tomd --workdir=" + natural4_dir + " --uuiddir=" + uuid + "/" + spreadsheet_id + "/" + sheet_id + " " + target_path
-        print("hello.py child: calling natural4-exe (%s) (slowly) for tomd" % (natural4_exe), file=sys.stderr)
-        print("hello.py child: %s" % (create_files), file=sys.stderr)
-        nl4exe = subprocess.run([create_files], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        print("hello.py child: back from slow natural4-exe 1 (took", datetime.datetime.now() - start_time, ")",
-              file=sys.stderr)
-        print("hello.py child: natural4-exe stdout length = %d" % len(nl4exe.stdout.decode('utf-8')), file=sys.stderr)
-        print("hello.py child: natural4-exe stderr length = %d" % len(nl4exe.stderr.decode('utf-8')), file=sys.stderr)
+    print("hello.py child: returning at", datetime.datetime.now(), "(total", datetime.datetime.now() - start_time,
+          ")", file=sys.stderr)
 
-        print("hello.py child: returning at", datetime.datetime.now(), "(total", datetime.datetime.now() - start_time,
-              ")", file=sys.stderr)
-
-        # ---------------------------------------------
-        # Postprocessing:
-        # Turn textual natural4 files generated by Maude transpiler into interactive
-        # HTML visualizations of the state space.
-        # ---------------------------------------------
-        maude_path = Path(uuid_ss_folder) / 'maude'
-        maude_path.mkdir(parents=True, exist_ok=True)
-        natural4_file = maude_path / 'LATEST.natural4'
-        natural4_rules = None
-        with open(natural4_file) as f:
-            natural4_rules = f.read()
+    # ---------------------------------------------
+    # Postprocessing:
+    # Turn textual natural4 files generated by Maude transpiler into interactive
+    # HTML visualizations of the state space.
+    # ---------------------------------------------
+    maude_path = Path(uuid_ss_folder) / 'maude'
+    maude_path.mkdir(parents=True, exist_ok=True)
+    natural4_file = maude_path / 'LATEST.natural4'
+    natural4_rules = None
+    with open(natural4_file) as f:
+        natural4_rules = f.read()
 
   # We don't proceed with post processing if the natural4 file is empty or
   # contains only whitespaces.
@@ -321,32 +321,12 @@ async def process_csv():
           )
         )
 
-        # threading.Thread(
-        #   target = maude_vis.config_to_html_file,
-        #   args = (
-        #     maude_main_mod,
-        #     config,
-        #     'all *',
-        #     maude_path / 'LATEST_state_space.html'
-        #   )
-        # ).start()
-
-        # threading.Thread(
-        #   target = maude_vis.natural4_rules_to_race_cond_htmls,
-        #   args = (
-        #     maude_main_mod,
-        #     maude_path / 'LATEST_race_cond.html',
-        #     natural4_rules
-        #   )
-        # ).start()
-
       # this return shouldn't mean anything because we're in the child, but gunicorn may somehow pick it up?
       return json.dumps(response)
 
-    # ---------------------------------------------
-    # return to sidebar caller
-    # ---------------------------------------------
-
+  # ---------------------------------------------
+  # return to sidebar caller
+  # ---------------------------------------------
 
 # ################################################
 # run when not launched via gunicorn
