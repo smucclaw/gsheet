b360a855daf02982c988e739e58b3c4189ecc0dd
Joe Watt <joewatt95@u.nus.edu>
2023-06-06 23:24:25 +0800

    disable font option in pdf output

--- natural4-server/natural4_server/hello.py
+++ natural4-server/natural4_server/hello.py
@@ -240,7 +240,7 @@ def process_csv():
     # print(f'Markdown file {md_file} exists: {md_file.exists()}', file=sys.stderr)
     if md_file.exists():
       # md_file = str(md_file.absolute())
-      # print(f'Markdown file: {md_file}', file=sys.stderr)
+      print(f'Markdown file: {md_file}', file=sys.stderr)
       docx_path = Path(uuid_ss_folder) / 'docx'
       docx_path.mkdir(parents=True, exist_ok=True)
       docx_file = docx_path / f'{timestamp}.docx'
@@ -269,7 +269,7 @@ def process_csv():
         md_file, 'pdf', outputfile = str(pdf_file),
         extra_args = [
           '--pdf-engine=xelatex',
-          '-V', 'CJKmainfont="Droid Sans Fallback"',
+          # '-V', 'CJKmainfont="Droid Sans Fallback"',
           '-f', 'markdown+hard_line_breaks',
           '-s',
         ]
