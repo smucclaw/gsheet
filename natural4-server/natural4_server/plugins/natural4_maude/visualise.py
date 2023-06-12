983840f39473cfd067cf51434267656bfbc20321
Joe Watt <joewatt95@u.nus.edu>
2023-06-12 19:35:53 +0800

    t

--- natural4-server/natural4_server/plugins/natural4_maude/visualise.py
+++ natural4-server/natural4_server/plugins/natural4_maude/visualise.py
@@ -356,8 +356,7 @@ def rewrite_graph_to_edge_pairs(rewrite_graph):
     # Compute the transitive closure of the transition relation, stopping once
     # we reach the fixed point (if it exists < omega).
     iterate(one_step_transition),
-    take(1000),
-    filter(lambda state : state['is_fixed_point']),
+    filter(lambda state : state['seen_ids'] <= 200 and state['is_fixed_point']),
     first,
     get('edge_pairs')
   )
