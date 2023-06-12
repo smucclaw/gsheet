0ca9dbbe75c8de5e82bd775a9291a126e1c2a74e
Joe Watt <joewatt95@u.nus.edu>
2023-06-12 17:44:15 +0800

    t

--- natural4-server/natural4_server/hello.py
+++ natural4-server/natural4_server/hello.py
@@ -334,8 +334,11 @@ async def process_csv(request: Request) -> HTTPResponse:
       'vue_purs_task': vue_purs_task
     }:
       v8k_url = f':{v8k_port}{v8k_base_url}'
-      if vue_purs_task:
-        await add_tasks_to_background(app, [vue_purs_task])
+      match vue_purs_task:
+        case {'func': func, 'args': args}:
+          app.add_task(func(*args))
+        # await add_tasks_to_background(app, [vue_purs_task])
+        case _: pass
     case _:
       v8k_url = None
 
