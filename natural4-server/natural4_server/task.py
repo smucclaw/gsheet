bae24f7df29293aa4c30df0e7f9c855f0a60c216
Joe Watt <joewatt95@u.nus.edu>
2023-06-12 10:46:56 +0800

    t

--- natural4-server/natural4_server/task.py
+++ natural4-server/natural4_server/task.py
@@ -46,10 +46,15 @@ async def add_tasks_to_background(
     match task:
       case {'func': func, 'args': args}:
         print(f'Adding background task: {task}', file=sys.stderr)
-        if inspect.iscoroutinefunction(func):
-          task = func(*args)
-        else:
-          task = asyncio.to_thread(func, *args)
-        app.run_after_response(task)
+        # if inspect.iscoroutinefunction(func):
+        #   task = func(*args)
+        # else:
+        #   task = asyncio.to_thread(func, *args)
+
+        @app.task
+        def _func(args):
+          func(*args)
+
+        app.run_after_response(_func(args))
 
         # app.add_background_task(func, *args)
