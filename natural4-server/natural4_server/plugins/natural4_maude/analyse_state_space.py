f2b691cf30749d1283d3463259d392badf6ea8ca
Joe Watt <joewatt95@u.nus.edu>
2023-06-12 13:50:20 +0800

    t

--- natural4-server/natural4_server/plugins/natural4_maude/analyse_state_space.py
+++ natural4-server/natural4_server/plugins/natural4_maude/analyse_state_space.py
@@ -23,47 +23,32 @@ maude_main_file: Path = Path('plugins') / 'natural4_maude' / 'main.maude'
 maude_main_mod = init_maude_n_load_main_file(maude_main_file)
 
 @curry
-def gen_state_space(
+def gen_state_space_and_find_race_cond(
   output_path: str | os.PathLike,
-  config: maude.Term
-):
-  '''
-  Generate state space graph.
-  Note that graph.expand() in FailFreeGraph may take forever because the state
-  space may be infinite.
-  '''
-
-  return asyncio.run(
-    run_tasks(
-      aiostream.stream.just(
-        asyncio.to_thread(
-          config_to_html_file,
-          maude_main_mod, config, 'all *',
-          Path(output_path) / 'LATEST_state_space.html'
-        )
-      )
-    )
-  )
-
-@curry
-def find_race_cond(
-  output_path: str | os.PathLike,
+  config: maude.Term,
   natural4_rules: str
 ):
   '''
-  Find a trace with race conditions and generate a graph.
+  Generate state space graph and find a trace with race conditions.
+  Note that the former may take forever because it uses graph.expand() in FailFreeGraph,
+  which doesn't terminate when the state space may be infinite.
   '''
 
   return asyncio.run(
     run_tasks(
-      aiostream.stream.just(
+      aiostream.stream.iterate([
         asyncio.to_thread(
           natural4_rules_to_race_cond_htmls,
           maude_main_mod,
           Path(output_path) / 'LATEST_race_cond.html',
           natural4_rules
+        ),
+        asyncio.to_thread(
+          config_to_html_file,
+          maude_main_mod, config, 'all *',
+          Path(output_path) / 'LATEST_state_space.html'
         )
-      )
+      ])
     )
   )
 
@@ -83,11 +68,11 @@ async def get_maude_tasks(
   output_path.mkdir(parents=True, exist_ok=True)
   # natural4_file = maude_path / 'LATEST.natural4'
   async with aiofile.async_open(natural4_file) as f:
-    natural4_rules: str = await f.read()
+    natural4_rules: str = (await f.read()).strip()
 
   # We don't proceed with post processing if the natural4 file is empty or
   # contains only whitespaces.
-  if natural4_rules.strip():
+  if natural4_rules:
     # Transform the set of rules into the initial configuration of the
     # transition system.
     config: maude.Term | None = natural4_rules_to_config(
@@ -96,21 +81,6 @@ async def get_maude_tasks(
     # Do we need to worry about this being None?
     if config:
       yield Task(
-        func = gen_state_space,
-        args = (output_path, config)
-        # func = natural4_rules_to_race_cond_htmls,
-        # args = (
-        #   maude_main_mod,
-        #   Path(output_path) / 'LATEST_race_cond.html',
-        #   natural4_rules
-        # )
-      )
-      yield Task(
-        func = find_race_cond,
-        args = (output_path, natural4_rules)
-        # func = config_to_html_file,
-        # args = (
-        #   maude_main_mod, config, 'all *',
-        #   Path(output_path) / 'LATEST_state_space.html'
-        # )
+        func = gen_state_space_and_find_race_cond,
+        args = (output_path, config, natural4_rules)
       )
