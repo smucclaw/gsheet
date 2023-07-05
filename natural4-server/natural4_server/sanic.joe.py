7205b9d2795b396ac5500bcd4d4ded40896d7025
Ruslan Khafizov <ruslan.khafizov@gmail.com>
2023-07-05 15:51:31 +0800

    Improve TLS config
    Sanic knows how to fetch proper certs from letsencrypt folder.
    Previous config didn't include full cert chain causing issues with
    curl

--- natural4-server/natural4_server/sanic.joe.py
+++ natural4-server/natural4_server/sanic.joe.py
@@ -14,15 +14,10 @@ load_dotenv(stream = StringIO(raw_env))
 
 from hello import app
 
-ssl: dict[str, str] = {
-  'cert': '/etc/letsencrypt/live/cclaw.legalese.com/cert.pem',
-  'key': '/etc/letsencrypt/live/cclaw.legalese.com/privkey.pem'
-}
-
 if __name__ == '__main__':
   app.run(
     host = '0.0.0.0', port = 8200,
     fast = True,
     access_log = False,
-    ssl = ssl
+    ssl = '/etc/letsencrypt/live/cclaw.legalese.com/'
   )
