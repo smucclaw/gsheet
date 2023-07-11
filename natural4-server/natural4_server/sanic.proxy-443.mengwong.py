be249bf13cf416baccd64e03a9a265f8c635c02b
Meng Weng Wong <mwwong@smu.edu.sg>
2023-07-11 13:55:24 +0800

    mengwong prod 8090

---
+++ natural4-server/natural4_server/sanic.proxy-443.mengwong.py
@@ -0,0 +1,23 @@
+from io import StringIO
+from dotenv import load_dotenv
+
+# Define and load environment variables.
+raw_env: str = '''
+basedir=.
+V8K_WORKDIR=/home/mengwong/v8kworkdir-8090
+v8k_startport=8091
+'''
+
+load_dotenv(stream = StringIO(raw_env))
+
+from hello import app
+
+
+if __name__ == '__main__':
+  app.run(
+    host = '0.0.0.0',
+    port = 8090,
+    fast = True,
+    access_log = False,
+#    ssl = '/etc/letsencrypt/live/cclaw.legalese.com/'
+  )
