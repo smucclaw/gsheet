bc58ceddd082aaf63a0ce72e7320245efebb6c7f
joewatt95 <joewatt95@u.nus.edu>
2023-09-26 11:41:48 +0800

    fix catching of exc

--- natural4-server/natural4_server/hello.py
+++ natural4-server/natural4_server/hello.py
@@ -67,7 +67,10 @@ natural4_exe: str = os.environ.get('natural4_exe', default_filenm_natL4exe_from_
 
 try:
     nl4exe_time_limit: float = float(os.environ['NL4EXE_TIME_LIMIT'])
-except ValueError:
+except:
+    # Here we're catching:
+    # KeyError that could arise in accessing NL4EXE_TIME_LIMIT from the env
+    # ValueError that could occur when casting that to a float.
     nl4exe_time_limit: float = 20
 
 # sometimes it is desirable to override the default name
