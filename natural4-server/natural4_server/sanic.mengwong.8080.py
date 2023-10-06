a064159d96a4463b7a2463d66f28dff86646f768
Meng Weng Wong <mwwong@smu.edu.sg>
2023-10-06 20:03:34 +0800

    for mengwong to run sanic 8080 on dev.cclaw.legalese.com (#89)

---
+++ natural4-server/natural4_server/sanic.mengwong.8080.py
@@ -0,0 +1,33 @@
+from io import StringIO
+from dotenv import load_dotenv
+
+# Define and load environment variables.
+raw_env: str = '''
+basedir=.
+V8K_WORKDIR=/home/mengwong/v8kworkdir-8080
+v8k_startport=8081
+v8k_path=/home/mengwong/src/smucclaw/vue-pure-pdpa/bin/v8k
+natural4_exe=natural4-unstable
+CCLAW_HTTPS="true, set in sanic.*.py"
+'''
+
+
+
+
+
+load_dotenv(stream=StringIO(raw_env))
+
+from hello import app
+
+ssl: dict[str, str] = {
+    'cert': '/etc/letsencrypt/live/dev.cclaw.legalese.com/cert.pem',
+    'key': '/etc/letsencrypt/live/dev.cclaw.legalese.com/privkey.pem'
+}
+
+if __name__ == '__main__':
+    app.run(
+        host='0.0.0.0', port=8080,
+        fast=True,
+        access_log=False,
+#        ssl=ssl
+    )
