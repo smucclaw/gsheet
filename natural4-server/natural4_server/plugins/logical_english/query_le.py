1f5fd81fb227d979ab850e374a893c7df374d988
Joe Watt <joewatt95@u.nus.edu>
2023-11-16 21:42:45 +0800

    cleanup imports

--- natural4-server/natural4_server/plugins/logical_english/query_le.py
+++ natural4-server/natural4_server/plugins/logical_english/query_le.py
@@ -1,15 +1,15 @@
+import anyio
 import asyncio
-import pathlib
+import os
 
 import janus_swi as janus
 
-_le_qlf_path: pathlib.Path = pathlib.Path('plugins') / 'logical_english' / 'le.qlf'
+_le_qlf_path: os.PathLike = anyio.Path('plugins') / 'logical_english' / 'le.qlf'
 janus.consult(f'{_le_qlf_path}')
 
 # TODO: Implement query_le_with_new_data
 def _query_le(query_params: dict[str, str]) -> str:
   janus.attach_engine()
-  print(f'Current Prolog engine: {janus.engine()}')
 
   result = janus.apply_once(
     'le_answer', 'parse_en_and_query_and_explanation',
