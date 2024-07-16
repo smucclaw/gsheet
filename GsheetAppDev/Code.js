d23e79ec4feb82867893084c9de0e51cdac59fd5
Ruslan Khafizov <ruslan.khafizov@gmail.com>
2024-07-16 16:40:15 +0800

    Remove functions defined elsewhere

--- GsheetAppDev/Code.js
+++ GsheetAppDev/Code.js
@@ -134,56 +134,6 @@ function getSsid() {
   return [spreadsheetId, sheetId];
 }
 
-function exportCSV(uuid, spreadsheetId, sheetId) {
-  let sheet = SpreadsheetApp.getActiveSheet();
-  Logger.log("exportCSV: initialized. constructing CSV.");
-  let cellArraysOfText = sheet.getDataRange().getDisplayValues();
-  let csvStr = cellArraysToCsv(cellArraysOfText);
-  // ui.prompt(csvStr);
-  
-  let formData = {
-    'uuid': uuid,
-    'spreadsheetId': spreadsheetId,
-    'sheetId': sheetId,
-    'csvString': csvStr
-  };
-  let options = {
-    'method' : 'post',
-    'payload' : formData
-  };
-  Logger.log("exportCSV: calling UrlFetchApp");
-
-  let response = UrlFetchApp.fetch(url_hp() + '/post', options);
-  Logger.log("exportCSV: after UrlFetchApp");
-  return response.getContentText();
-}
-
-function cellArraysToCsv(rows) {
-  const regex = /"/g;
-  let csvStr = "";
-  for (let i = 0; i < rows.length; i++) {
-    let row = "";
-    for (let j = 0; j < rows[i].length; j++) {
-      var needToEnclose = false;
-      if (rows[i][j]) {
-        let comma = (rows[i][j].indexOf(",") > -1);
-        let change = rows[i][j].replace(regex, '\"\"');
-        needToEnclose   = comma || change;
-        if (needToEnclose) {
-          row = row + '"' + change + '"';
-        }
-        else {
-          row = row + change;
-        }
-      }
-      row = row + ",";
-    }
-    row = row.substring(0, (row.length-1));
-    csvStr += row + '\n';
-  }
-  return csvStr;
-}
-
 function saveUuid() {
   let userCache = CacheService.getUserCache();
   let cached = "";
