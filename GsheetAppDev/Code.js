7ada1a644257c3ae48183d5c0980a818f5cbc057
Ruslan Khafizov <ruslan.khafizov@gmail.com>
2024-07-16 14:40:04 +0800

    Remove functions which were moved to CommonLib

--- GsheetAppDev/Code.js
+++ GsheetAppDev/Code.js
@@ -72,7 +72,7 @@ function showSidebar() {
   Logger.log(`workDirUrl = ` + workDirUrl);
   let sidebar = HtmlService.createTemplateFromFile('main');
   Logger.log("calling exportCSV()");
-  sidebar.fromFlask = JSON.parse(exportCSV(cachedUuid, spreadsheetId, sheetId));
+  sidebar.fromFlask = JSON.parse(CommonLib.exportCSV(url_hp() + '/post', cachedUuid, spreadsheetId, sheetId));
   Logger.log("fromFlask returned");
   Logger.log(sidebar.fromFlask);
   sidebar.native_url          = workDirUrl + "native/LATEST.hs";
@@ -132,58 +132,6 @@ function getSsid() {
   }
   console.log("getSsid: " + spreadsheetId + " / "+ sheetId)
   return [spreadsheetId, sheetId];
-}
-
-function exportCSV(uuid, spreadsheetId, sheetId) {
-  let sheet = SpreadsheetApp.getActiveSheet();
-  Logger.log("exportCSV: initialized. constructing CSV.");
-  let cellArraysOfText = sheet.getDataRange().getDisplayValues();
-  let csvStr = cellArraysToCsv(cellArraysOfText);
-  // ui.prompt(csvStr);
-  
-  let formData = {
-    'name': 'Max Loo',
-    'email': 'maxloo@smu.edu.sg',
-    'uuid': uuid,
-    'spreadsheetId': spreadsheetId,
-    'sheetId': sheetId,
-    'csvString': csvStr
-  };
-  let options = {
-    'method' : 'post',
-    'payload' : formData
-  };
-  Logger.log("exportCSV: calling UrlFetchApp");
-
-  let response = UrlFetchApp.fetch(url_hp() + '/post', options);
-  Logger.log("exportCSV: after UrlFetchApp");
-  return response.getContentText();
-}
-
-function cellArraysToCsv(rows) {
-  const regex = /"/g;
-  let csvStr = "";
-  for (let i = 0; i < rows.length; i++) {
-    let row = "";
-    for (let j = 0; j < rows[i].length; j++) {
-      var needToEnclose = false;
-      if (rows[i][j]) {
-        let comma = (rows[i][j].indexOf(",") > -1);
-        let change = rows[i][j].replace(regex, '\"\"');
-        needToEnclose   = comma || change;
-        if (needToEnclose) {
-          row = row + '"' + change + '"';
-        }
-        else {
-          row = row + change;
-        }
-      }
-      row = row + ",";
-    }
-    row = row.substring(0, (row.length-1));
-    csvStr += row + '\n';
-  }
-  return csvStr;
 }
 
 function saveUuid() {
