f2f66bbb659e93524d9450d068c2f6e134a8aa30
Ruslan Khafizov <ruslan.khafizov@gmail.com>
2024-09-26 15:57:11 +0800

    Add workdir and aasvg endpoints

---
+++ natural4-server/natural4_server/fastapi.py
@@ -0,0 +1,32 @@
+import logging
+import os
+import sys
+import anyio
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+
+
+basedir = anyio.Path(os.environ.get("basedir", "."))
+
+template_dir: anyio.Path = basedir / "template"
+temp_dir: anyio.Path = basedir / "temp"
+static_dir: anyio.Path = basedir / "static"
+natural4_dir: anyio.Path = anyio.Path(
+    os.environ.get("NL4_WORKDIR", temp_dir / "workdir")
+)
+
+logger = logging.getLogger('uvicorn.error')
+
+app = FastAPI()
+
+app.mount("/workdir", StaticFiles(directory=natural4_dir), name="static")
+
+@app.get("/aasvg/{uuid}/{ssid}/{sid}/{image}")
+async def show_aasvg_image(uuid: str, ssid: str, sid: str, image: str) -> FileResponse:
+    print("show_aasvg_image: handling request for /aasvg/ url", file=sys.stderr)
+
+    image_path = natural4_dir / uuid / ssid / sid / "aasvg" / "LATEST" / image
+    print(f"show_aasvg_image: sending path {image_path}", file=sys.stderr)
+
+    return FileResponse(image_path)
