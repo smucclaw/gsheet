f3c04e70931ab57e4017439672f5a07e17228ea2
Ruslan Khafizov <ruslan.khafizov@gmail.com>
2024-09-30 17:30:37 +0800

    Tests for static resources

--- natural4-server/tests/test_endpoint.py
+++ natural4-server/tests/test_endpoint.py
@@ -1,7 +1,14 @@
 from sanic import Sanic
+import pytest
 
+@pytest.mark.asyncio
+async def test_live(app: Sanic):
+    request, response = await app.asgi_client.get("/health/liveness")
 
-def test_sample(app: Sanic):
-    request, response = app.test_client.get("/")
+    assert response.status == 200
 
-    #assert response.status == 200
+@pytest.mark.asyncio
+async def test_aasvg(app: Sanic):
+    request, response = await app.asgi_client.get("/aasvg/787aa580-7c24-4b3e-bcc6-3b1c627153f7/1GdDyNl6jWaeSwY_Ao2sA8yahQINPcnhRh9naGRIDGak/1206725099/CoveredIf-full.svg")
+
+    assert response.status == 200
