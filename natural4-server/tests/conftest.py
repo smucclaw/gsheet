b839d0f1a1897de72905d107c14e553f2219afb2
Ruslan Khafizov <ruslan.khafizov@gmail.com>
2024-10-02 10:39:02 +0800

    Add e2e post test (#214)

--- natural4-server/tests/conftest.py
+++ natural4-server/tests/conftest.py
@@ -6,3 +6,11 @@ from natural4_server.hello import app as application_instance
 @pytest.fixture
 def app():
     return application_instance
+
+@pytest.fixture
+def post_data():
+    return {
+        "uuid": "e909063f-f7a2-4e6a-945c-f1b21314227d",
+        "spreadsheetId": "1GdDyNl6jWaeSwY_Ao2sA8yahQINPcnhRh9naGRIDGak",
+        "sheetId":"1206725099"
+    }
--- natural4-server/tests/test_endpoint.py
+++ natural4-server/tests/test_endpoint.py
@@ -1,7 +1,18 @@
 from sanic import Sanic
+import pytest
 
+@pytest.mark.asyncio
+async def test_live(app: Sanic):
+    request, response = await app.asgi_client.get("/health/liveness")
 
-def test_sample(app: Sanic):
-    request, response = app.test_client.get("/")
+    assert response.status == 200
 
-    #assert response.status == 200
+@pytest.mark.asyncio
+async def test_post(app: Sanic, post_data):
+    with open('tests/data/rodents.csv') as f:  
+        input_data = f.read()
+
+    post_data['csvString'] = input_data
+    request, response = await app.asgi_client.post('/post', data=post_data)
+
+    assert response.status == 200
