66ee09ba903a7e035817f578f0638ef1fbf530fd
Ruslan Khafizov <ruslan.khafizov@gmail.com>
2024-10-18 16:29:33 +0800

    Test file copy

--- natural4-server/tests/test_endpoint.py
+++ natural4-server/tests/test_endpoint.py
@@ -1,3 +1,7 @@
+from asyncio import sleep
+import json
+import os
+import re
 from sanic import Sanic
 import pytest
 
@@ -15,4 +19,13 @@ async def test_post(app: Sanic, post_data):
     post_data['csvString'] = input_data
     request, response = await app.asgi_client.post('/post', data=post_data)
 
+    await sleep(1)
+
+    response_json = json.loads(response.body)
+    v8k_url = response_json["v8k_url"]
+    qq = re.compile(r"/webapp/809(\d)/")
+    slot_str = qq.search(v8k_url).group(1)
+    slot = int(slot_str)-1
+    
+    print(f"V8K_WORKDIR = {os.environ['V8K_WORKDIR']}/vue-{slot:02}")
     assert response.status == 200
