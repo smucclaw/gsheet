8d068a20ad46e19a7996e751d90afc373ab308b3
Ruslan Khafizov <ruslan.khafizov@gmail.com>
2025-01-13 12:52:42 +0800

    Add e2e tests for aajson and pdf (#244)
    * Add e2e tests for aajson and pdf
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- natural4-server/tests/test_endpoint.py
+++ natural4-server/tests/test_endpoint.py
@@ -1,11 +1,15 @@
+from time import sleep, time
 from sanic import Sanic
 from sanic_testing.reusable import ReusableClient
-from time import sleep
+from sanic.application.constants import ServerStage
 
 def test_post(app: Sanic, post_data):
     with open('tests/data/rodents.csv') as f:
         input_data = f.read()
         with ReusableClient(app) as client:
+            for info in app.state.server_info:
+                info.stage = ServerStage.SERVING
+
             post_data['csvString'] = input_data
             request, response_post = client.post('/post', data=post_data)
             assert response_post.status == 200
@@ -25,3 +29,14 @@ def test_post(app: Sanic, post_data):
 
             request, response_json = client.get(f'{workdir_url}/petri/LATEST-small.png')
             assert response_json.status == 200
+
+            start_time = time()
+            while time() - start_time < 60:
+                print('Waiting for PDF...')
+                request, response_pdf = client.get(f'{workdir_url}/pdf/LATEST.pdf')
+                if response_pdf.status != 200:
+                    sleep(5)
+                else:
+                    break
+
+            assert response_pdf.status == 200
